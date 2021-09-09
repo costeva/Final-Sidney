0cab4ba3e3172d5dbef32f55503a5c0e4c46a670
costeva <67351701+costeva@users.noreply.github.com>
2021-09-09 01:31:33 -0300

    a
    a

---
+++ datos.php
@@ -0,0 +1,13 @@
+<?php
+
+$nombre= $_POST['nombre'];
+$apellido= $_POST['apellido'];
+$tel= $_POST['telefono'];
+$email= $_POST['email'];
+$asunto= $_POST['asunto'];
+$mensaje= $_POST['consulta'];
+
+header("Location:gracias.html");
+
+
+?>
